46e8b61623338e16e00da1ced16c24edfbee0a41
henrique <bongador@protonmail.com>
2022-11-25 11:10:56 -0600

    Updated webrct nodes

--- src/hooks/useIPFS.js
+++ src/hooks/useIPFS.js
@@ -23,11 +23,11 @@ function useIpfs() {
 
                         // Websocket:
                         // '/dns4/ws-star-signal-2.servep2p.com/tcp/443//wss/p2p-websocket-star',
-                        '/dns4/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
-                        '/dns6/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
-                        //'/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-                        //'/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-                        //'/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
+                        //'/dns4/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
+                        //'/dns6/star.thedisco.zone/tcp/9090/wss/p2p-webrtc-star',
+                        '/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+                        '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+                        '/dns4/webrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star/',
                         // Local signal server
                         //'/ip4/127.0.0.1/tcp/4711/ws/p2p-websocket-star'
                         //
